dac2f6191262cdf3c323e251c7d43d37fc208dd5
mrandr1an <krackedissad@gmail.com>
2025-06-14 22:23:48 +0300

    Chore: rewritting the parsing tree

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use frontend::parser::{tree::{Expr, Let}, KaisaParser, Rule};
-use crate::frontend::parser::tree::Value;
-use pest::Parser;
-
 mod frontend;
 mod backend;
 
 
 fn main()
 {
-    match KaisaParser::parse(Rule::assignment,"let type Animal name = name*int")
-    {
-	Ok(res) => println!("{:#?}",Let::parse_let(res)),
-	Err(err) => println!("{:#?}",err),
-    }
 }
