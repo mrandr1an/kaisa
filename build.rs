5cb5903ba3ba50b6b20c30419a264da318830f5f
chris <chris@kallap.168.2.15>
2025-06-28 20:17:53 +0300

    Chore: Compartmentalize lalrpop files, better integration with logos.
    Grammars and their attributes go in src/frontend/grammars.
    
    Prologue (use statements, anything that might come up) in
    src/frontend/prologue.
    
    Externs that is used by external tokenizer is in src/frontend/extern/

--- build.rs
+++ build.rs
@@ -1,30 +1,45 @@
-use std::env;
-use std::fs::{read_to_string,write};
+use std::{fs, io};
+use std::fs::{read_to_string, write};
 use std::path::Path;
+
+fn read_grammars_to_string<P: AsRef<Path>>(dir: P) -> io::Result<String> {
+    let mut contents = String::new();
+
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if let Some(file_name) = path.file_name().and_then(|n| n.to_str()) {
+            if file_name.ends_with(".grammar") {
+		contents.push_str(format!("\n// BEGIN {}\n",file_name).as_str());
+                contents.push_str(&fs::read_to_string(&path)?);
+		contents.push_str(format!("\n// END {}\n",file_name).as_str());
+                contents.push('\n'); 
+            }
+        }
+    }
+
+    Ok(contents)
+}
 
 fn main()
 {
-    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let grammar_files = [
-	"src/frontend/grammars/value.grammar",
-	"src/frontend/grammars/expr.grammar",
-    ];
+    let lalrpop_prolog_path = "src/frontend/lalrpop/prologue";
+    let lalrpop_grammars_path = "src/frontend/lalrpop/grammars/";
+    let lalrpop_extern = "src/frontend/lalrpop/extern/logos.tokens";
 
-    let mut kaisa_grammar = String::new();
-
-    for file in grammar_files.iter()
+    if let Ok(mut prologue) = read_to_string(lalrpop_prolog_path)
+        &&
+       let Ok(external) = read_to_string(lalrpop_extern)
+	&&
+       let Ok(grammars) = read_grammars_to_string(lalrpop_grammars_path)
+	
     {
-	let file_path = Path::new(&manifest_dir).join(file);
-	let content = read_to_string(&file_path)
-	    .unwrap_or_else(|e| panic!("Failed to read grammar file: {}, {e}", file));
-	kaisa_grammar.push_str(&format!("// BEGIN {}\n",file));
-	kaisa_grammar.push_str(&content);
-	kaisa_grammar.push_str("\n");
-	kaisa_grammar.push_str(&format!("// END {}\n",file));
+	prologue.push('\n');
+	prologue.push_str(external.as_str());
+	prologue.push_str(grammars.as_str());
+	let out_path = Path::new("src/kaisa.lalrpop");
+        write(out_path,&prologue).unwrap_or_else(|e| panic!("Failed to write kaisa grammar: {}",e));
+	lalrpop::process_root().unwrap();
     }
-
-    let out_path = Path::new(&manifest_dir).join("src/kaisa.lalrpop");
-    write(out_path,&kaisa_grammar).unwrap_or_else(|e| panic!("Failed to write kaisa grammar: {}",e));
-
-    lalrpop::process_root().unwrap();
 }
