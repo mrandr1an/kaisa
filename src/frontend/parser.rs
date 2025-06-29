8c30e0679299c52c20f83d2f70f67868095f3772
chris <chris@kallap.168.2.15>
2025-06-29 09:27:32 +0300

    Feat: Add string literal to parser

--- src/frontend/parser.rs
+++ src/frontend/parser.rs
@@ -6,6 +6,7 @@ lalrpop_mod!(
     kaisa
 );
 
+
 #[cfg(test)]
 mod tests
 {
@@ -19,5 +20,13 @@ mod tests
 	let input = "23";
 	let res = ValueParser::new().parse(input,KaisaLexer::new(input));
 	assert_eq!(res,Ok(Value::Number(Node::new(0..2,"23"))));
+    }
+
+    #[test]
+    fn parse_string()
+    {
+	let input = "\"Hello world!\"";
+	let res = ValueParser::new().parse(input,KaisaLexer::new(input));
+	assert_eq!(res,Ok(Value::String(Node::new(0..14,"\"Hello world!\""))));
     }
 }
